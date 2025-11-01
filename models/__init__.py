6433f8b857992c9ffd13c0e9e3b0f5b5ce12e2ef
Jonatan Stiven Gutierrez Nieto <mariaeugenianieto345@gmail.com>
2025-11-01 17:46:14 -0500

    ...

--- models/__init__.py
+++ models/__init__.py
@@ -1,3 +1,7 @@
+# El archivo __init__.py es un archivo especial en Python que indica que una carpeta debe ser tratada como un paquete.
+# Su función principal es inicializar el paquete al momento de ser importado, pudiendo incluir código de configuración o 
+# definir qué módulos y clases estarán disponibles mediante la variable __all__.
+
 from .library_models import Book, User, LibraryStore
 
 __all__ = ['Book', 'User', 'LibraryStore']
